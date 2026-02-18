79b76d0eae3a135fc736d26949c2596a272f861f
Edge <edge@nire.com>
2026-02-19 02:41:07 +0900

    デザイン調整

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './style.css'
 import { Game } from './game.js'
 
+// iOS Safari workaround: enables :active pseudo-class on touch
+document.addEventListener('touchstart', () => {}, { passive: true });
+
 document.addEventListener('DOMContentLoaded', () => {
   const game = new Game();
   game.init();
